682b9444043aaa39bc3e7ea73eeff8ff9ff2db73
Mr.Salih <salihbaz07@gmail.com>
2023-01-05 12:07:00 +0300

    get_next_line

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: spirnaz <spirnaz@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/04 16:31:20 by spirnaz           #+#    #+#             */
-/*   Updated: 2023/01/04 16:31:21 by spirnaz          ###   ########.fr       */
+/*   Created: 2023/01/04 16:30:45 by spirnaz           #+#    #+#             */
+/*   Updated: 2023/01/04 16:30:46 by spirnaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 3
+#  define BUFFER_SIZE 2
 # endif
 
-char	*get_next_line(int fd);
-char	*ft_get_str(int fd, char *str);
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
 
-int		ft_strchr(char *str, int c);
-int		ft_strlen(char *str);
-char	*ft_strjoin(char *str, char *temp_str);
-char	*ft_get_line(char *str);
-char	*ft_get_new_str(char *str);
+char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+//UTILS
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
