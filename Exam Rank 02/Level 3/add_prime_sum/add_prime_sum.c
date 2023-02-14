47a4d04b041619c910f5233a681f6eef06329395
Mr.Salih <71170042+salihprnz07@users.noreply.github.com>
2023-02-14 17:14:02 +0300

    add_prime_sum
    my code

--- Exam Rank 02/Level 3/add_prime_sum/add_prime_sum.c
+++ Exam Rank 02/Level 3/add_prime_sum/add_prime_sum.c
@@ -1,36 +1,78 @@
 #include <unistd.h>
 
-int is_prime(int num)
-{
-	int i = 3;
-	
-	if (num <= 1)
-		return (0);
-	if (num % 2 == 0 && num > 2)
-		return (0);
-	while (i < (num / 2))
-	{
-		if (num % i == 0)
-			return 0;
-		i += 2;
-	}
-	return 1;
+void	ft_putchar(char c) {
+	write(1, &c, 1);
 }
 
-int main(int argc, char *argv[])
-{
-	int sum = 0;
-	int nb = ft_atoi(argv[1]);
-	
-	if (argc == 2)
-	{
-		while (nb > 0)
-			if (is_prime(nb--))
-				sum += (nb + 1);
-		ft_putnbr(sum);
+void	ft_putnbr(int nb) {
+	if (nb < 0) {
+		ft_putchar('-');
+		nb = -nb;
 	}
-	if (argc != 2)
-		ft_putnbr(0);
-	write(1, "\n", 1);
-	return (0);
+	if (nb >= 10) {
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	if (nb < 10) ft_putchar(nb + 48);
+}
+
+int		ft_atoi(const char *str)
+{
+	int i = 0;
+	int n= 1;
+	int res = 0;
+
+	while (str[i] <= 32)
+		i++;
+	if (str[i] == '-')
+	{
+		n = -1;
+		i++;
+	}
+	if (str[i] == '+')
+		i++;
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - 48);
+		i++;
+	}
+	return (res * n);
+}
+
+int prime(int num)
+{
+    int i = 3;
+
+    if (num < 2 || num % 2 == 0)
+        return 0;
+    if (num == 2)
+        return 2;
+    while (i < num)
+        {
+            if (num % i == 0)
+                return 0;
+            i += 2;
+        }
+        return i;
+        
+}
+
+int main(int ac, char **av)
+{
+    int sum = 0;
+    int num = ft_atoi(av[1]);
+    
+    if (ac == 2)
+    {    
+        while(num > 2)
+            if(prime(num--))
+                sum += num + 1;
+        sum += 2;
+        ft_putnbr(sum);
+    }
+    else
+        write (1, "0", 1);
+
+    write (1, "\n", 1);
+    return 0;
 }
