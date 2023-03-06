ad08a13c26e32196351885e0c751ffbf5f8c8399
Mr.Salih <salihbaz07@gmail.com>
2023-03-06 12:41:57 +0300

    Level04/fprime

--- Exam Rank 02/Level 4/fprime/fprime.c
+++ Exam Rank 02/Level 4/fprime/fprime.c
@@ -29,6 +29,37 @@ int	main(int argc, char *argv[])
 	return (0);
 }
 
+/*
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		return 0;
+
+	argv++;
+	int result = 1;
+	int i = 1;
+	// printf("%s\n",*argv);
+	int arg = atoi(*argv);
+	// printf("%d\n",arg);
+	if (arg == 1)
+		printf("1");
+	while (arg >= i)
+	{
+		i++;
+		if (arg % i == 0)
+		{
+			printf("%d", i);
+			if(arg == i)
+				break;
+			printf("*");
+			arg /= i;
+			i = 1;
+		}
+	}
+}
+*/
+
+
 /* #include <stdio.h>
 #include <stdlib.h>
 
