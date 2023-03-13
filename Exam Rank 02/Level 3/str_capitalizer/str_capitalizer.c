e348dbb0873ee073d851443d9ee389043c2358ff
salihprnz07 <salihbaz07@gmail.com>
2023-03-13 14:56:07 +0300

    str_capitalizer new clean code

--- Exam Rank 02/Level 3/str_capitalizer/str_capitalizer.c
+++ Exam Rank 02/Level 3/str_capitalizer/str_capitalizer.c
@@ -1,5 +1,34 @@
 #include <unistd.h>
 
+void capitalize(char *str)
+{
+int i = 0;
+
+		while (str[i])
+		{
+			if(str[i - 1] == '\t' || str[i - 1] == ' ' || str[i - 1] == '\0')
+			{	if(str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+			}
+			else if (str[i] >= 'A' && str[i] <= 'Z')
+					str[i] += 32;
+			write(1, &str[i++], 1);
+		}
+		write (1, "\n", 1);
+}
+
+int main(int ac, char **av)
+{
+	int i = 1;
+
+	if (ac < 2)
+		write(1, "\n", 1);
+
+	while(i < ac)
+		capitalize(av[i++]);
+}
+
+/*
 void	str_capitalizer(char *str)
 {
 	int i = 0;
@@ -36,3 +65,4 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+*/
