87bd0c02bee45995f0dcb06dd130560012a162a8
Mr.Salih <salihbaz07@gmail.com>
2023-03-28 01:19:59 +0300

    Update rev_wstr.c

--- Exam Rank 02/Level 4/rev_wstr/rev_wstr.c
+++ Exam Rank 02/Level 4/rev_wstr/rev_wstr.c
@@ -33,3 +33,35 @@ int main(int argc, char **argv)
 	}
 	write(1, "\n", 1);
 }
+
+/*
+#include <unistd.h>
+
+void rev_wstr(char *s)
+{
+    int i = 0, j = 0;
+    while (s[j])
+        j++;
+    j--;
+    while (j >= 0)
+    {
+        while (j >= 0 && s[j] == ' ')
+            j--;
+        i = j;
+        while (i >= 0 && s[i] != ' ')
+            i--;
+        write(1, s + i + 1, j - i);
+        if (i > 0)
+            write(1, " ", 1);
+        j = i;
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+        rev_wstr(av[1]);
+    write(1, "\n", 1);
+    return 0;
+}
+*/
