c6f5379f05eed552db199accebdecd90da39dc96
salihprnz07 <salihbaz07@gmail.com>
2023-03-29 03:45:58 +0300

    epur end expand_str fixed

--- Exam Rank 02/Level 3/epur_str/epur_str.c
+++ Exam Rank 02/Level 3/epur_str/epur_str.c
@@ -1,29 +1,23 @@
 #include <unistd.h>
 
-int main(int argc, char const *argv[])
+int main(int ac, char **av)
 {
-	int i;
-	int flg;
+    int i = 0;
 
-	if (argc == 2)
-	{
-		i = 0;
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
-			i += 1;
-		while (argv[1][i])
-		{
-			if (argv[1][i] == ' ' || argv[1][i] == '\t')
-				flg = 1;
-			if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
-			{
-				if (flg)
-					write(1, " ", 1);
-				flg = 0;
-				write(1, &argv[1][i], 1);
-			}
-			i += 1;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+    if(ac == 2)
+    {
+        while(av[1][i] == ' ')
+            i++;
+        while(av[1][i])
+        {
+            if(!(av[1][i] != ' ' && av[1][i]))
+                i++;
+            write(1, &av[1][i++], 1);
+            if(av[1][i] == ' ')
+                write(1, " ", 1);
+            while(av[1][i] == ' ')
+                i++;
+        }
+    }
+    write(1, &av[1][i], 1);
 }
--- Exam Rank 02/Level 3/expand_str/expand_str.c
+++ Exam Rank 02/Level 3/expand_str/expand_str.c
@@ -1,57 +1,23 @@
 #include <unistd.h>
 
-int main(int argc, char const *argv[])
+int main(int ac, char **av)
 {
-    int i;
-    int flag;
+    int i = 0;
 
-    if (argc == 2)
+    if(ac == 2)
     {
-        i = 0;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+        while(av[1][i] == ' ')
             i++;
-        while (argv[1][i])
+        while(av[1][i])
         {
-            if (argv[1][i] == ' ' || argv[1][i] == '\t')
-                flag = 1;
-            if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
-            {
-                if (flag)
-                    write(1, "   ", 3);
-                flag = 0;
-                write(1, &argv[1][i], 1);
-            }
-            i++;
+            if(!(av[1][i] != ' ' && av[1][i]))
+                i++;
+            write(1, &av[1][i++], 1);
+            if(av[1][i] == ' ')
+                write(1, " ", 1);
+            while(av[1][i] == ' ')
+                i++;
         }
     }
-    write(1, "\n", 1);
-    return (0);
+    write(1, &av[1][i], 1);
 }
-/* 
-#include <unistd.h>
-
-int		main(int ac, char **av)
-{
-	int i;
-
-	i = 0;
-	if (ac == 2)
-	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
-			i++;
-		while (av[1][i] != '\0')
-		{
-			if (av[1][i] == ' ' || av[1][i] == '\t')
-			{
-				if (av[1][i + 1] > 32 && av[1][i + 1] != '\0')
-					write(1, "   ", 3);
-			}
-			else if (av[1][i] != ' ' && av[1][i] != '\t')
-				write(1, &av[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
-}
- */
